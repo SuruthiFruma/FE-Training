517d6c893f680004f77cc611a854c9bea933a27e
SuruthiFruma <suruthi.prabhu@anywhere.co>
2024-12-13 16:38:50 +0530

    FE: added user response

--- next-js-app/app/about.tsx
+++ next-js-app/app/about.tsx
@@ -1,28 +1,31 @@
 "use client";
-const AboutHeader = () => (
-  <div className="w-3/4">
-    <div className="h-1/4 flex flex-col">
-      <div className="flex justify-between">
-        <div className="flex">
-          <span className="w-6 h-6 rounded-full bg-zinc-500">A</span>
-          <div className="flex flex-col">
-            <h2>Adam Carter</h2>
-            <span>Portland US</span>
-          </div>
-        </div>
-        <div></div>
-      </div>
-      <div className="flex flex-wrap">
-        <nav>
-          <a>About</a>
-          <a>Notes</a>
-          <a>Appointments</a>
-          <a>Updates</a>
-        </nav>
-      </div>
-    </div>
 
-    <div className="h-3/4"></div>
-  </div>
+import { Box, Track } from "@adaptavant/eds-core";
+
+const AboutHeader = () => (
+  //   <Box as="div" className="w-3/4">
+  //     <Box as="div" className="h-1/4 flex flex-col">
+  //       <Box as="div" className="flex justify-between">
+  //         <Box as="div" className="flex">
+  //           <span className="w-6 h-6 rounded-full bg-zinc-500">A</span>
+  //           <Box as="div" className="">
+  //             <h2>Adam Carter</h2>
+  //             <span>Portland US</span>
+  //           </Box>
+  //         </Box>
+  //       </Box>
+  //       <Box as="div" className="flex flex-wrap">
+  //         <nav>
+  //           <a>About</a>
+  //           <a>Notes</a>
+  //           <a>Appointments</a>
+  //           <a>Updates</a>
+  //         </nav>
+  //       </Box>
+  //     </Box>
+
+  //     <Box as="div" className="h-3/4"></Box>
+  //   </Box>
+  <Track></Track>
 );
 export default AboutHeader;
--- next-js-app/app/page.tsx
+++ next-js-app/app/page.tsx
@@ -7,55 +7,101 @@ import {
   Box,
   Field,
   SearchInput,
+  Avatar,
+  Text,
+  TextLink,
+  DropdownMenu,
+  DropdownMenuPopover,
+  DropdownMenuTrigger,
+  DropdownMenuItem,
+  DropdownMenuList,
 } from "@adaptavant/eds-core";
 import AboutHeader from "./about";
+import { useEffect, useState } from "react";
 
-const Home = () => (
-  <Box as="div" className="flex h-full">
-    <Box as="div" className="w-1/4 p-7">
-      <Box as="div" className="h-1/4 space-y-8">
-        <Track
-          railEnd={
-            <IconButton
-              aria-label="Click me!"
-              icon={AddIcon}
-              variant="accentPrimary"
-            />
-          }
-          className="justify-between"
-        >
-          <Heading as="h1" className="text-heading-16">
-            Customers
-          </Heading>
-        </Track>
+const Home = () => {
+  const [userData, setUserData] = useState([]);
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const { default: userResponse } = await import(
+        "../util/usersResponse.json"
+      );
+      console.log(userResponse);
+      setUserData(userResponse);
+    };
+    fetchUserData();
+  }, []);
+  return (
+    <Box as="div" className="flex h-full">
+      <Box as="div" className="w-1/4 p-7">
+        <Box as="div" className="h-1/4 space-y-8">
+          <Track
+            railEnd={
+              <IconButton
+                aria-label="Click me!"
+                icon={AddIcon}
+                variant="accentPrimary"
+              />
+            }
+            className="justify-between"
+          >
+            <Heading as="h1" className="text-heading-16">
+              Customers
+            </Heading>
+          </Track>
 
-        <Track railEnd={<span>Options</span>}>
-          <Box as="div">
-            <span className="mr-8">All</span>
-            <span>Groups</span>
-          </Box>
-        </Track>
-
-        <Field labelVisibility="hidden" size="standard">
-          <SearchInput placeholder="Search" />
-        </Field>
-      </Box>
-
-      <Box as="div" className="h-3/4 overflow-y-scroll space-y-4">
-        {Array.from({ length: 30 }, (_, i) => i + 1).map((item) => {
-          return (
-            <Box as="div" className="flex gap-4" key={item}>
-              <Box as="div" className="w-6 h-6 rounded-full bg-gray-400">
-                <span className="text-white">S</span>
-              </Box>
-              <span>Name {item}</span>
+          <Track
+            className="justify-between"
+            railEnd={
+              <DropdownMenu>
+                <DropdownMenuTrigger>Options</DropdownMenuTrigger>
+                <DropdownMenuPopover>
+                  <DropdownMenuList>
+                    <DropdownMenuItem onClick={() => {}}>
+                      Option 1
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => {}}>
+                      Option 2
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onClick={() => {}}>
+                      Option 3
+                    </DropdownMenuItem>
+                  </DropdownMenuList>
+                </DropdownMenuPopover>
+              </DropdownMenu>
+            }
+          >
+            <Box as="div" className="flex gap-4">
+              <Text as="span" className="text-body-12">
+                <TextLink variant="accentPrimary" href="#">
+                  All
+                </TextLink>
+              </Text>
+              <Text as="span" className="text-body-12">
+                <TextLink variant="accentPrimary" href="#">
+                  Groups
+                </TextLink>
+              </Text>
             </Box>
-          );
-        })}
-      </Box>
-    </Box>
+          </Track>
 
-    <AboutHeader />
-  </Box>
-);
+          <Field labelVisibility="hidden" size="standard">
+            <SearchInput placeholder="Search" />
+          </Field>
+        </Box>
+
+        <Box as="div" className="h-3/4 overflow-y-scroll space-y-4">
+          {userData.map(user)=>(
+            <Box as="div" className="flex gap-4" key={user}>
+               <Avatar name={user} size="24" />
+               <span className="text-body-12">{user}</span>
+            </Box>
+
+          )}
+        </Box>
+      </Box>
+      <AboutHeader />
+    </Box>
+  )
+};
 export default Home;
